dc2135159b6334a96f67ba5792875f1880b0963c
Gunjan <glunkad007@gmail.com>
2023-10-11 11:40:43 +0530

    some fixes

--- frontend/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filterName, handleFilterChanged }) => {
-    return (
-        <div>
-            filter shown with <input value={filterName} onChange={handleFilterChanged} />
-        </div>
-    )
-}
-
-export default Filter 
--- frontend/src/components/Person.jsx
+++
@@ -1,9 +0,0 @@
-const Person = ({ name, number, deletePerson }) => {
-    return (
-        <div>
-            {name} {number} &nbsp;
-            <button onClick={deletePerson}>delete</button>
-        </div>
-    )
-}
-export default Person 
